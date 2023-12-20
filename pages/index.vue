1fde3eabbcb83890d4cda191605cd0eb06f6d98d
刘1300 <8641121+liu-1300@user.noreply.gitee.com>
2023-12-20 15:04:28 +0800

    引入图标

--- pages/index.vue
+++ pages/index.vue
@@ -3,5 +3,12 @@
         <div>
             <nuxt-link to="/test">index</nuxt-link>
         </div>
+        <n-icon size="40">
+            <Search/>
+        </n-icon>
     </div>
 </template>
+<script setup>
+    import { NIcon } from 'naive-ui';
+    import {Search} from '@vicons/ionicons5'
+</script>
