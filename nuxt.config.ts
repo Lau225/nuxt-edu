9cef6766393794507fada81838610c3d3096860c
刘1300 <8641121+liu-1300@user.noreply.gitee.com>
2023-12-20 10:47:40 +0800

    初始化项目完成

---
+++ nuxt.config.ts
@@ -0,0 +1,23 @@
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  build: {
+    transpile:
+      process.env.NODE_ENV === 'production'
+        ? [
+            'naive-ui',
+            'vueuc',
+            '@css-render/vue3-ssr',
+            '@juggle/resize-observer'
+          ]
+        : ['@juggle/resize-observer']
+  },
+  vite: {
+    optimizeDeps: {
+      include:
+        process.env.NODE_ENV === 'development'
+          ? ['naive-ui', 'vueuc', 'date-fns-tz/formatInTimeZone']
+          : []
+    }
+  }
+})
