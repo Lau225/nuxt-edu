e753e5737da34c04ff624738a887e4dca0132f7e
刘1300 <8641121+liu-1300@user.noreply.gitee.com>
2023-12-21 12:15:47 +0800

    错误信息的加载

---
+++ components/Loading/Group.vue
@@ -0,0 +1,34 @@
+
+import type { setup } from '@css-render/vue3-ssr';
+<template>
+  <div>
+    <template v-if="pending"> 加载中... </template>
+    <template v-else-if="error">
+      <n-result status="500" title="错误提示" :description="error.message">
+        <template #footer>
+          <n-button @click="$route.go(-1)">回到首页</n-button>
+        </template>
+      </n-result>
+    </template>
+    <template v-else>
+      <slot />
+    </template>
+  </div>
+</template>
+
+<script setup>
+import {NResult,NButton} from 'naive-ui'
+defineProps({
+  pedding: {
+    type: Boolean,
+    default: false,
+  },
+  error: {
+    type: [String, Boolean, Symbol],
+    default: "",
+  },
+});
+</script>
+
+<style>
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,38 @@
 <template>
-    <div>
-        <template v-if="pending">
-            加载中...
-        </template>
-        <template v-else-if="error">
-            {{ error?.data?.data }}
-        </template>
-        <template v-else>
-            <template v-for="(item,index) in data" :key="index">
-                <Banner :data="item.data" v-if="item.type === 'swiper'"></Banner>
-                <ImageView :data="item.data" v-else-if="item.type === 'icons'"></ImageView>
-                <ImageAd :data="item.data" v-else-if="item.type === 'imageAd'"></ImageAd>
-                <ListCard :title="item.title" :data="item.data" v-else-if="item.type === 'list'"></ListCard>
-                <ListCard :type="item.listType" :title="item.title" :data="item.data" v-else-if="item.type === 'promotion'"></ListCard>
-            </template>
-        </template>
-    </div>
+  <LoadingGroup :pedding="pending" :error="error">
+    <template v-for="(item, index) in data" :key="index">
+      <Banner :data="item.data" v-if="item.type === 'swiper'"></Banner>
+      <ImageView
+        :data="item.data"
+        v-else-if="item.type === 'icons'"
+      ></ImageView>
+      <ImageAd :data="item.data" v-else-if="item.type === 'imageAd'"></ImageAd>
+      <ListCard
+        :title="item.title"
+        :data="item.data"
+        v-else-if="item.type === 'list'"
+      ></ListCard>
+      <ListCard
+        :type="item.listType"
+        :title="item.title"
+        :data="item.data"
+        v-else-if="item.type === 'promotion'"
+      ></ListCard>
+    </template>
+  </LoadingGroup>
 </template>
 <script setup>
-    useHead({
-        title:'课堂首页',
-        meta:[
-        {name:"description",content:"首页描述"},
-        {name:"keywords",content:"首页关键词"}
-      ],
-    })
+useHead({
+  title: "课堂首页",
+  meta: [
+    { name: "description", content: "首页描述" },
+    { name: "keywords", content: "首页关键词" },
+  ],
+});
 
-    const {data,pending,refresh,error} = await useIndexDataApi()
-    // 服务端报错
-    if(process.server && error.value){
-        showError(error.value?.data?.data)
-    }
+const { data, pending, refresh, error } = await useIndexDataApi();
+// 服务端报错
+if (process.server && error.value) {
+  showError(error.value?.data?.data);
+}
 </script>
