dddb7f5e60df8b6679132b6aa249f22249f390c0
刘1300 <8641121+liu-1300@user.noreply.gitee.com>
2023-12-21 08:40:08 +0800

    公共底部开发

--- components/PageFooter.vue
+++ components/PageFooter.vue
@@ -1,6 +1,15 @@
 <template>
-  <div>
-    footer
+  <div class="mt-auto bg-dark-400 text-gray-100">
+    <div class="flex items-center justify-center py-5">
+        <a href="#" v-for="i in 5" :key="i" class="p-3">
+          友情链接
+        </a>
+    </div>
+    <div class="container border-t border-gray-700 flex items-center justify-center py-5">
+      <small>
+        Copyright @2022 dishait.cn 
+      </small>
+    </div>
   </div>
 </template>
 
