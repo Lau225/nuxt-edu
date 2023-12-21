62dbbe7547057d9c3c0b8b4e4720401c9e7673be
刘1300 <8641121+liu-1300@user.noreply.gitee.com>
2023-12-21 08:52:00 +0800

    设置响应拦截器

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,17 @@
 <template>
     <div>
-        首页
+        {{ data }}
     </div>
 </template>
 <script setup>
-
+    const {data} = await useFetch('http://demonuxtapi.dishait.cn/pc/index',{
+        key:"IndexData",
+        headers:{
+            appid:"bd9d01ecc75dbbaaefce"
+        },
+        // 响应之前的数据处理
+        transform:(res) => {
+            return res.data
+        } 
+    })
 </script>
