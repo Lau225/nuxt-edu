c3fd92c2b6741d1a52a2b4de6bc6f78367687243
刘1300 <8641121+liu-1300@user.noreply.gitee.com>
2023-12-21 14:47:38 +0800

    搜索弹框组件完成

--- components/SearchBar.vue
+++ components/SearchBar.vue
@@ -2,7 +2,7 @@
   <n-drawer v-model:show="active" placement="top">
     <div class="h-full flex justify-center items-center">
       <n-input-group class="flex justify-center items-center">
-        <n-input size="large" v-model="keyword" placeholder="请输入关键词" :style="{ width: '500px' }" />
+        <n-input @keyup.enter="handleClick" size="large" v-model:value="keyword" placeholder="请输入关键词" :style="{ width: '500px' }" />
         <n-button size="large" @click="handleClick" :disabled="!keyword" type="primary" ghost> 搜索 </n-button>
       </n-input-group>
     </div>
@@ -17,8 +17,25 @@ const open = () => {
   active.value = true;
 };
 
+const close = () => {
+    keyword.value = ""
+    active.value = false
+}
+
 const handleClick = () => {
-    console.log("搜索");
+    setTimeout(() => {
+        close()
+    }, 100);
+    navigateTo({
+        name:"search-type-page",
+        params:{
+            type:"course",
+            page:1
+        },
+        query:{
+            keyword:keyword.value
+        }
+    })
 }
 
 defineExpose({
