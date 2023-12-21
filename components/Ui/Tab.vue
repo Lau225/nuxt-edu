b37cac894ce14ab6de592d1edfda5e4760dd9f47
刘1300 <8641121+liu-1300@user.noreply.gitee.com>
2023-12-21 15:31:26 +0800

    自定义tab组件完成

---
+++ components/Ui/Tab.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="tab">
+    <slot/>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.tab{
+    @apply w-full flex overflow-x-auto bg-white mb-4 rounded;
+}
+</style>
--- pages/search/[type]/[page].vue
+++ pages/search/[type]/[page].vue
@@ -1,10 +1,42 @@
 <template>
   <div>
-    {{ $route.params.type }} {{ $route.params.page }}
+    <h5 class=" text-lg md-3">
+        "{{ title }}"的搜索和结果
+    </h5>
+    <UiTab>
+        <UiTabItem @click="hanleClick(item.value)" :active="item.value === type" v-for="item,index in tabs" :key="index">
+            {{ item.label }}
+        </UiTabItem>
+    </UiTab>
   </div>
 </template>
 
 <script setup>
+const route = useRoute()
+const title = route.query.keyword
+useHead(title)
+const tabs = [
+    {
+        label:"课程",
+        value:"course"
+    },
+    {
+        label:"专栏",
+        value:"column"
+    }
+]
+const type = ref(route.params.type)
+const hanleClick = (t) => {
+    navigateTo({
+        params:{
+            ...route.params,
+            type:t
+        },
+        query:{
+            ...route.query
+        }
+    })
+}
 definePageMeta({
     middleware:["search"]
 })
